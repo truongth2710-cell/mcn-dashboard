20500f647b69f7b9d14cb2daf709f03a2dcc82f4
truongth2710-cell <truongth2710@gmail.com>
2025-11-24 02:00:58 +0700

    Update main.tsx

--- mcn-frontend/src/main.tsx
+++ mcn-frontend/src/main.tsx
@@ -16,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       authorizationParams={{
         redirect_uri: window.location.origin,
         audience,
+        scope: "openid profile email"  // yêu cầu thêm email vào token
       }}
       cacheLocation="localstorage"
     >
